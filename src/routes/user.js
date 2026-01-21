23507caefcf2a0190ad922e27b7ffa2e90893074
LuongHuongGiang20236027 <giang.lh236027@sis.hust.edu.vn>
2026-01-21 11:08:48 +0700

    hi

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,5 @@
 import express from "express"
-import {
-    updateProfile,
-    updatePassword,
-    forgotPassword,
-    resetPasswordByToken
-} from "../controllers/userController.js";
-
+import { updateProfile, updatePassword } from "../controllers/userController.js"
 import { authMiddleware } from "../middleware/authMiddleware.js"
 import { upload } from "../middleware/uploadMiddleware.js";
 const router = express.Router()
@@ -13,8 +7,5 @@ const router = express.Router()
 router.put("/profile", authMiddleware, upload.single("avatar"), updateProfile);
 // Đổi mật khẩu
 router.put("/password", authMiddleware, updatePassword);
-
-router.post("/forgot-password", forgotPassword);
-router.post("/reset-password", resetPasswordByToken);
 
 export default router
